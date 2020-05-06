7e111c64db1d4c2211e523cce9ac94f9e016e3de
Elazar Gershuni <elazarg@gmail.com>
2020-05-06 21:23:09 +0300

    cli

---
+++ naqdan.py
@@ -0,0 +1,28 @@
+from tensorflow.keras.models import load_model
+
+import utils
+import dataset
+import hebrew
+
+model = load_model('models/modern.h5')
+
+
+def naqdan(data: dataset.Data) -> str:
+    prediction = model.predict(data.normalized)
+    [actual_niqqud, actual_dagesh, actual_sin] = [dataset.from_categorical(prediction[0]), dataset.from_categorical(prediction[1]), dataset.from_categorical(prediction[2])]
+    actual = dataset.merge(data.text, ts=data.normalized, ns=actual_niqqud, ds=actual_dagesh, ss=actual_sin)
+    return '.\n'.join(' '.join(actual).split('. '))
+
+
+def diacritize_text(text):
+    return naqdan(dataset.Data.from_text(hebrew.iterate_dotted_text(text), 82))
+
+
+def diacritize_file(input_filename='-', output_filename='-'):
+    text = naqdan(dataset.load_file(input_filename, 82))
+    with utils.smart_open(output_filename, 'w', encoding='utf-8') as f:
+        f.write(text)
+
+
+if __name__ == '__main__':
+    diacritize_file('texts/modern/newspapers/1.txt')
