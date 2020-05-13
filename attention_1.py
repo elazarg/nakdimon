e78c5bc8c130c20268116828fb0308dd10a65aef
Elazar Gershuni <elazarg@gmail.com>
2020-05-14 01:37:33 +0300

    save toy attentions

---
+++ attention_1.py
@@ -0,0 +1,72 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+from keras_multi_head import MultiHeadAttention
+
+
+class TransformerBlock(layers.Layer):
+    def __init__(self, embed_dim, num_heads, ff_dim, rate=0.1):
+        super(TransformerBlock, self).__init__()
+        self.att = MultiHeadAttention(num_heads)
+        self.ffn = keras.Sequential(
+            [layers.Dense(ff_dim, activation="relu"), layers.Dense(embed_dim),]
+        )
+        self.layernorm1 = layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = layers.LayerNormalization(epsilon=1e-6)
+        self.dropout1 = layers.Dropout(rate)
+        self.dropout2 = layers.Dropout(rate)
+
+    def call(self, inputs, training):
+        attn_output = self.att(inputs)
+        # attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.layernorm1(inputs + attn_output)
+        ffn_output = self.ffn(out1)
+        # ffn_output = self.dropout2(ffn_output, training=training)
+        return self.layernorm2(out1 + ffn_output)
+
+
+class TokenAndPositionEmbedding(layers.Layer):
+    def __init__(self, maxlen, vocab_size, emded_dim, mask_zero=False):
+        super().__init__()
+        self.token_emb = layers.Embedding(input_dim=vocab_size, output_dim=emded_dim, mask_zero=mask_zero)
+        self.pos_emb   = layers.Embedding(input_dim=maxlen,     output_dim=emded_dim, mask_zero=mask_zero)
+
+    def call(self, x):
+        maxlen = tf.shape(x)[-1]
+        return self.token_emb(x) + self.pos_emb(tf.range(start=0, limit=maxlen, delta=1))
+
+
+def example():
+    vocab_size = 20000  # Only consider the top 20k words
+    maxlen = 200  # Only consider the first 200 words of each movie review
+    (x_train, y_train), (x_val, y_val) = keras.datasets.imdb.load_data(num_words=vocab_size)
+    print(len(x_train), "Training sequences")
+    print(len(x_val), "Validation sequences")
+    x_train = keras.preprocessing.sequence.pad_sequences(x_train, maxlen=maxlen)
+    x_val = keras.preprocessing.sequence.pad_sequences(x_val, maxlen=maxlen)
+
+    embed_dim = 32  # Embedding size for each token
+    num_heads = 2  # Number of attention heads
+    ff_dim = 32  # Hidden layer size in feed forward network inside transformer
+
+    inputs = layers.Input(shape=(maxlen,))
+    embedding_layer = TokenAndPositionEmbedding(maxlen, vocab_size, embed_dim)
+    x = embedding_layer(inputs)
+    transformer_block = TransformerBlock(embed_dim, num_heads, ff_dim)
+    x = transformer_block(x)
+    x = layers.GlobalAveragePooling1D()(x)
+    x = layers.Dropout(0.1)(x)
+    x = layers.Dense(20, activation="relu")(x)
+    x = layers.Dropout(0.1)(x)
+    outputs = layers.Dense(2, activation="softmax")(x)
+
+    model = keras.Model(inputs=inputs, outputs=outputs)
+
+    model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
+    history = model.fit(
+        x_train, y_train, batch_size=32, epochs=2, validation_data=(x_val, y_val)
+    )
+
+if __name__ == '__main__':
+    example()
